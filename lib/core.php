e1aac204652b9caad4a07bebeef33aacf0c0c536
elzaer <elliott.fox@gmail.com>
2018-11-14 15:51:36 +1000

    Create core.php
    setting up the console object and useful functions library

--- lib/core.php
+++ lib/core.php
@@ -1,4 +1,21 @@
 <?php
+Namespace Core
 /** LIBRARY CORE CLASS **/
 
-function is_prime($n){for($i=$n;--$i&&$n%$i;);return$i==1;}
+# PRIME NUMBER TEST
+function is_prime($n) 
+{
+  for($i=$n;--$i&&$n%$i;);
+  return $i==1;
+}
+
+# EVEN NUMBER TEST
+function is_even($n)
+{
+  return $n%2==0;
+}
+
+class Console extends DBConfig\DB
+{
+  
+}
